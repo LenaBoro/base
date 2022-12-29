3d01b52426e6cc79523dd0414be4d26bc2946ccc
Lena <>
2022-12-30 00:11:46 +0300

    4 navigator

--- 4-navigator/4-navigator.js
+++ 4-navigator/4-navigator.js
@@ -3,6 +3,5 @@ const adressLat = 2;
 const adressLong = 3;
 const positionLat = 1;
 const positionLong = 4;
-
 const res = Math.sqrt((adressLong - adressLat) ** 2 + (positionLong - positionLat) ** 2);
 console.log(res);
