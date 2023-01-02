66eba3c1246f8ff7b4e893f40d76f46a0133dda0
Lena <>
2023-01-02 14:03:00 +0300

    add function sort

---
+++ 9-sort-loops/index.js
@@ -0,0 +1,14 @@
+const arr = [-1, 40, -5, -100, 3, 15];
+
+function sortArr(arr) {
+    for (let i = 0; i <= arr.length; i++) {
+        for (let y = 0; y <= arr.length; y++) {
+            if (arr[y] > arr[i]) {
+                [arr[i], arr[y]] = [arr[y], arr[i]]
+            }
+        }
+    }
+    console.log(arr)
+    return arr;
+}
+sortArr(arr);
