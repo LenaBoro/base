148084933523acc29a41d79fe0acc5f5648582c1
Lena <>
2023-01-02 21:21:10 +0300

    add =

--- 6-driver/index.js
+++ 6-driver/index.js
@@ -2,4 +2,4 @@ let age = 30;
 let isDrunk = false;
 let hasLicense = false;
 
-age > 18 && !hasLicense && !isDrunk ? console.log('может') : console.log('не может'); 
+age >= 18 && !hasLicense && !isDrunk ? console.log('может') : console.log('не может'); 
