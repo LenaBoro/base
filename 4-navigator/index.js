88333021ba5cc82a520528ed28ec3178b6d0fd65
Lena <>
2023-01-02 21:27:02 +0300

    fix formula

--- 4-navigator/index.js
+++ 4-navigator/index.js
@@ -3,5 +3,7 @@ const adressLat = 2;
 const adressLong = 3;
 const positionLat = 1;
 const positionLong = 4;
-const res = Math.sqrt((adressLong - adressLat) ** 2 + (positionLong - positionLat) ** 2);
+const xy1 = adressLat - positionLat;
+const xy2 = adressLong - positionLong;
+const res = Math.sqrt((xy1) ** 2 + (xy2) ** 2);
 console.log(res);
