c780bc96073009344263768b9d096077093943cb
Lena <>
2023-01-02 14:03:42 +0300

    add quotes

--- 9-sort-loops/index.js
+++ 9-sort-loops/index.js
@@ -4,11 +4,11 @@ function sortArr(arr) {
     for (let i = 0; i <= arr.length; i++) {
         for (let y = 0; y <= arr.length; y++) {
             if (arr[y] > arr[i]) {
-                [arr[i], arr[y]] = [arr[y], arr[i]]
+                [arr[i], arr[y]] = [arr[y], arr[i]];
             }
         }
     }
-    console.log(arr)
+    console.log(arr);
     return arr;
 }
 sortArr(arr);
