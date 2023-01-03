c5633057d8ca624e7bc78db368245f9cd34de5dc
Lena <>
2023-01-03 20:49:53 +0300

    fix

--- 9-sort-loops/index.js
+++ 9-sort-loops/index.js
@@ -1,14 +1,13 @@
 const arr = [-1, 40, -5, -100, 3, 15];
 
 function sortArr(arr) {
-    for (let i = 0; i <= arr.length; i++) {
-        for (let y = 0; y <= arr.length; y++) {
-            if (arr[y] > arr[i]) {
-                [arr[i], arr[y]] = [arr[y], arr[i]];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[i]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
             }
         }
     }
-    console.log(arr);
     return arr;
 }
 sortArr(arr);
