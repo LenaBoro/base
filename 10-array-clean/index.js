7a91a742adc1e3dff6c4042ad7165f4cd629b11e
Lena <>
2023-01-03 20:55:24 +0300

    fix

--- 10-array-clean/index.js
+++ 10-array-clean/index.js
@@ -2,16 +2,12 @@ const arr = [10, 2, 1, 5, 105, 4, 7, 99, 100];
 
 const removeItem = item => item > 5 ? true : false;
 function arrayClean(arr, fn) {
+    const answer = [];
     for (let i = 0; i < arr.length; i++) {
         let removeOrnot = fn(arr[i]);
-
-        if (removeOrnot) {
-            arr.splice(arr.indexOf(arr[i]), 1);
-            i--;
-        }
+        (removeOrnot) ? answer.push(arr[i]) : false;
     }
-    return arr;
+    return answer;
 }
 
 arrayClean(arr, removeItem);
-console.log(arr);
