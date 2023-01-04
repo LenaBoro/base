3a7540effce8f166c1a5514c243eb32cebb329b0
Lena <>
2023-01-04 20:06:01 +0300

    fix loop

--- 9-sort-loops/index.js
+++ 9-sort-loops/index.js
@@ -1,7 +1,7 @@
 const arr = [-1, 40, -5, -100, 3, 15];
 
 function sortArr(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[i]) {
                 [arr[i], arr[j]] = [arr[j], arr[i]];
