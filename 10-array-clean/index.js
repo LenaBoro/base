9dc1d98ee5a1fe4235fbed86853fa8794312441e
Lena <>
2023-01-04 20:09:32 +0300

    fix name of variable

--- 10-array-clean/index.js
+++ 10-array-clean/index.js
@@ -1,11 +1,14 @@
 const arr = [10, 2, 1, 5, 105, 4, 7, 99, 100];
 
 const removeItem = item => item > 5 ? true : false;
-function arrayClean(arr, fn) {
+
+const arrayClean = (arr, fn) => {
     const answer = [];
     for (let i = 0; i < arr.length; i++) {
-        let removeOrnot = fn(arr[i]);
-        (removeOrnot) ? answer.push(arr[i]) : false;
+        let isRemove = fn(arr[i]);
+        if (isRemove) {
+            answer.push(arr[i]);
+        }
     }
     return answer;
 }
