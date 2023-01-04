3f48cd0ffb2bce6042be60e3f351f527f96d480e
Lena <>
2023-01-04 21:23:13 +0300

    fix

--- 11-date-array/index.js
+++ 11-date-array/index.js
@@ -1,17 +1,37 @@
-const dateArr = ['10-01-2023', '10_022023', '10/02---2023', '10-00-1000', '10/00/2020'];
+const dateArr = ['00/01/2023', '10_022023', '10/02---2023', '10-00-1000', '10/00/2020'];
 
 function checkArraOfdate(arr) {
-    const formatData = arr.map((dataEl) => dataEl.split(/-|_|\//).join('-'))
-        .filter((dataEl) => {
-            let dataStr = dataEl.split('-');
-            let dateExmpl = new Date(dataStr[2], dataStr[1], dataStr[0]);
-            if (dateExmpl.getFullYear() == dataStr[2]
-                && dateExmpl.getMonth() == dataStr[1]
-                && dateExmpl.getDate() == dataStr[0]) {
-                return dataStr;
+    const formatData = arr.filter((dataEl) => {
+        if ((dataEl.length === 10) &&
+            (dataEl[2] === dataEl[5]) &&
+            (dataEl[2] === '-' || dataEl[2] === '/')) {
+            return dataEl;
+        }
+    });
+
+    const checkData = formatData.filter((dataEl) => {
+        if (dataEl[2] === '-') {
+            let dateExmpl1 = new Date(dataEl[2], dataEl[1], dataEl[0]);
+            let dateJoin = dataEl.split('-');
+            if (dateExmpl1.getFullYear() === dateJoin[2]
+                && (dateExmpl1.getMonth() === dateJoin[1])
+                && (dateExmpl1.getDate() === dateJoin[0])) {
+                return dataEl;
             }
-        })
-    return formatData;
+            return dataEl;
+        } else if (dataEl[2] == '/') {
+            let dateExmpl2 = new Date(dataEl[2], dataEl[0], dataEl[1]);
+            let dateJoin = dataEl.split('/')
+            if (dateExmpl2.getFullYear() === dateJoin[2]
+                && (dateExmpl2.getMonth() === dateJoin[0])
+                && (dateExmpl2.getDate() === dateJoin[1])) {
+                return dataEl;
+            }
+            return dataEl;
+        }
+    }).map(data => data.split(/-|_|\//).join('-'))
+
+    return checkData;
 }
 
 checkArraOfdate(dateArr);
