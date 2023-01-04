8d3acc71ae3fc28c756cd11bc81f484d8f5af61a
Lena <>
2023-01-04 13:12:43 +0300

    fix add optional chaining

--- 7-converter/index.js
+++ 7-converter/index.js
@@ -1,18 +1,29 @@
 
-const ExchangeRatesUSDandEUR = {
-    USD: 1.07,
-    EUR: 0.95,
-};
-const ExchangeRatesUSDandRUB = {
-    USD: 72,
-    RUB: 0.014
+const ExchangeRates = {
+    USD: {
+        RUB: 71,
+        USD: 1,
+        EUR: 0.94,
+    },
+    EUR: {
+        RUB: 77,
+        USD: 1.06,
+        EUR: 1,
+    },
+    RUB: {
+        RUB: 1,
+        USD: 0.014,
+        EUR: 0.013,
+    }
 };
 
-function convertRub(sum, objRate) {
-    const objKeysRate = Object.keys(objRate);
-    const [currency, targetСurrency] = objKeysRate
-    const converterCurrencytoTargetСurrency = () => sum / objRate[currency];
-    const converterTargetСurrencytoСurrency = () => sum * objRate[targetСurrency];
+function convert(sum, currency, targetСurrency) {
+    const rate = ExchangeRates[currency]?.[targetСurrency];
+    if (!rate) { return null }
+
+    const converterCurrencytoTargetСurrency = () => sum / rate;
+    const converterTargetСurrencytoСurrency = () => sum * rate;
+
     switch (`${currency}/${targetСurrency}`) {
         case (`${currency}/${targetСurrency}`):
             return converterCurrencytoTargetСurrency();
@@ -24,5 +35,4 @@ function convertRub(sum, objRate) {
             return null;
             break;
     }
-
 }
