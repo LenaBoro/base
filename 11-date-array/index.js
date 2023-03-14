f124a4c2e2aaa0debf39c1b109ff11f31a10a033
Lena <>
2023-03-14 10:48:15 +0300

    fix name variable, delete extra code

--- 11-date-array/index.js
+++ 11-date-array/index.js
@@ -2,29 +2,29 @@ const dateArr = ['ff/ff/2023', '10_022023', '10/02---2023', '10-01-1000', '10/01
 
 function transformFormateDate(dateStr) {
     let dateArray = dateStr.split('/');
-    let flagMMDDYYYYFormat = false;
+    let isMMDDYYYY = false;
 
     if (dateArray.length !== 3) {
         dateArray = dateStr.split('-');
-        flagMMDDYYYYFormat = true;
+        isMMDDYYYY = true;
     }
 
     if (dateArray.length !== 3) {
-        return null
+        return false;
     }
-    return isDateValid(dateArray, flagMMDDYYYYFormat);
+
+    return isDateValid(dateArray, isMMDDYYYY);
 }
 
-function isDateValid(dateArray, flagFormat) {
-    let [day, month, year] = [];
-    let date = '';
-    if (flagFormat) {
-        [day, month, year] = dateArray.map(dateEl => Number(dateEl));
-    } else {
+function isDateValid(dateArray, formatDate) {
+    let [day, month, year] = dateArray.map(dateEl => Number(dateEl));
+
+    if (formatDate) {
         [month, day, year] = dateArray.map(dateEl => Number(dateEl));
     }
-    date = new Date(year, month - 1, day);
-    return date.getFullYear() == year && date.getMonth() == month - 1 && date.getDate() == day;
+
+    let date = new Date(year, month - 1, day);
+    return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
 
 }
 
